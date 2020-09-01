e7a41e9221b8161ac67d47c48561e2b9dc4563f3
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-09-01 16:39:27 -0300

    Modified Product

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,13 +1,110 @@
 import React from "react";
 
-import { Container } from "./styles";
+import tshirtImage from "../../assets/tshirt.png";
+
+import {
+  Container,
+  Row,
+  Panel,
+  Column,
+  Gallery,
+  Section,
+  Description,
+} from "./styles";
 
 const Product: React.FC = () => {
   return (
     <Container>
-      <h1>Product</h1>
+      <Row>
+        <a href="#">Compartilhar</a>
+        <a href="#">Vender um igual</a>
+      </Row>
+
+      <Panel>
+        <Column>
+          <Gallery>
+            <img alt="T-Shirt" src={tshirtImage} />
+          </Gallery>
+
+          <Info />
+        </Column>
+
+        <Column>
+          {/* <ProductAction /> */}
+          {/* <SellerInfo /> */}
+
+          <WarrantySection />
+          <WarrantySection />
+          <WarrantySection />
+        </Column>
+      </Panel>
     </Container>
   );
 };
+
+const WarrantySection = () => (
+  <Section>
+    <h4>Garantia</h4>
+
+    <div>
+      <span>
+        <p className="title">Compra Garantida com o Lorem Ipsum</p>
+        <p className="description">
+          Receba o produto que está esperando ou devolvemos seu dinheiro
+        </p>
+      </span>
+      <span>
+        <p className="title">Garantia do vendedor</p>
+        <p className="description">sem garantia</p>
+      </span>
+    </div>
+
+    <a href="#">Saiba mais sobre garantia</a>
+  </Section>
+);
+
+const Info = () => (
+  <Description>
+    <h2>Descrição</h2>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et dolor
+      bibendum diam ullamcorper bibendum vitae a ex. Ut in ipsum lacus. Nullam
+      dictum nisi sem, sed sagittis arcu ultricies ut. Mauris sodales nisl
+      velit. Mauris quis velit at urna bibendum viverra. Pellentesque sodales
+      laoreet nunc, et blandit libero lobortis eget. Pellentesque pellentesque
+      sed mauris dictum porttitor.
+      <br />
+      <br />
+      Itens inclusos: <br />
+      - 1x LED <br />
+      - 1x LED <br />
+      - 1x LED <br />
+      - 1x LED <br />
+      - 1x LED <br />
+      <br />
+      Etiam semper a dui ac lacinia. Curabitur at nisi ut urna aliquet
+      venenatis. Nullam eget leo nec lectus euismod finibus vel in enim. Vivamus
+      sollicitudin nisi est, cursus rhoncus orci finibus in. Cras volutpat
+      sodales neque. Vestibulum ante ipsum primis in faucibus orci luctus et
+      ultrices posuere cubilia curae; Donec imperdiet lorem a efficitur viverra.
+      Donec consectetur orci id ex sollicitudin vulputate. Nulla eget sapien
+      tortor. Aenean iaculis pellentesque felis, facilisis molestie risus mollis
+      non. Proin tempus, nunc non pulvinar tristique, ligula risus porta odio,
+      non pulvinar elit nisi vitae velit. Morbi vulputate dolor eu tortor
+      iaculis ultricies. Aliquam accumsan aliquam cursus. Curabitur id egestas
+      dolor. Ut sed nulla vestibulum, condimentum lacus a, semper augue.
+      Vestibulum sagittis dui ut lorem scelerisque rhoncus. In id facilisis leo.
+      Sed porttitor faucibus fringilla. Aenean eget mi vitae est convallis
+      pretium eget sed quam. Phasellus purus magna, consequat id laoreet in,
+      blandit quis eros. Integer fringilla justo nisi, vitae vulputate erat
+      finibus et. Duis lacinia ultrices purus. Curabitur ipsum purus, lacinia ut
+      nisl nec, rutrum tristique ipsum. Duis luctus est sem, a laoreet quam
+      scelerisque eget. Phasellus justo ex, pellentesque et dignissim nec,
+      malesuada at justo. Cras porta vel lacus in ultricies. Curabitur id tempus
+      tellus. Pellentesque hendrerit massa sit amet vulputate varius.
+    </p>
+  </Description>
+);
 
 export default Product;
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -1,5 +1,104 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
+export const Row = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 16px 0;
+
+  > a {
+    font-size: 14px;
+    text-decoration: none;
+    color: var(--color-blue);
+    padding: 2.5px 0;
+
+    & + a {
+      padding-left: 10px;
+      margin-left: 10px;
+      border-left: 1px solid var(--color-border);
+    }
+  }
+`;
+
+export const Panel = styled.div`
+  background: var(--color-white);
+  box-shadow: var(--panel-shadow);
+
+  display: grid;
+  grid-template-columns: 65fr 35fr;
+`;
+
+export const Column = styled.div`
+  &:first-child {
+    border-right: 1px solid var(--color-border);
+  }
+`;
+
+export const Gallery = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 530px;
+
+  > img {
+    height: 73%;
+  }
+`;
+
+export const Section = styled.div`
+  border-top: 1px solid var(--color-border);
+  padding: 48px 32px;
+
+  display: flex;
+  flex-direction: column;
+
+  > h4 {
+    font-size: 18px;
+    margin-bottom: 40px;
+  }
+  > div {
+    display: flex;
+    flex-direction: column;
+
+    > span + span {
+      margin-top: 16px;
+    }
+    .title {
+      font-size: 16px;
+      color: var(--color-black);
+    }
+    .description {
+      margin-top: 5px;
+      font-size: 14px;
+      color: var(--color-gray);
+      line-height: 19px;
+    }
+  }
+  > a {
+    margin-top: 20px;
+    font-size: 14px;
+    font-weight: 600;
+    color: var(--color-blue);
+    text-decoration: none;
+  }
+`;
+
+export const Description = styled.div`
+  border-top: 1px solid var(--color-border);
+  padding: 44px 32px;
+
+  > h2 {
+    font-size: 24px;
+    margin-bottom: 32px;
+  }
+  > p {
+    font-size: 20px;
+    color: var(--color-gray);
+    line-height: 27px;
+  }
 `;
